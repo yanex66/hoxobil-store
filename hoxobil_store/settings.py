b9b2dda47ca806bd834d5d0910624148c7167757
yanexbil <yanex77.ii@gmail.com>
2026-09-11 01:26:22 +0100

    Update DATABASES config to use dj_database_url.config() with ssl_require=True for Render PostgreSQL

--- hoxobil_store/settings.py
+++ hoxobil_store/settings.py
@@ -156,7 +156,11 @@ DATABASE_URL = config('DATABASE_URL', default='')
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
